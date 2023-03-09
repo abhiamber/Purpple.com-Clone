57aa76a653004933d0153e4b4d1cd5fe57c58ab0
abhiamber191 <abhiamber191@gmail.com>
2023-03-09 23:11:31 +0530

    renderurl

--- frontend/src/BackendURL.js
+++ frontend/src/BackendURL.js
@@ -1,5 +1,6 @@
 // const BackendURL = "https://creepy-yak-wetsuit.cyclic.app";
 // const BackendURL = "https://purplle-t7vj.onrender.com";
-const BackendURL = "http://localhost:8080";
+// const BackendURL = "http://localhost:8080";
+const BackendURL = "https://abhishek-purpple.onrender.com";
 
 export default BackendURL;
